8545f0e1815a9cca6d288e88874e5c2a95f3335a
Ahmad Badrul Ulum <badrul@lenna.ai>
2021-07-23 05:54:12 +0700

    rollback migrate, change field name to username

--- app/Models/User.php
+++ app/Models/User.php
@@ -15,8 +15,12 @@ class User extends Authenticatable
      *
      * @var array
      */
-    protected $fillable = [
-        'name', 'email', 'password',
+    // protected $fillable = [
+    //     'name', 'email', 'password',
+    // ];
+
+    protected $guarded = [
+        'id',
     ];
 
     /**
